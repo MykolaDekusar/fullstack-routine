f014f677cdef18fff901c9668fba63095e95ad5d
Mykola Dekusar <ndekusar97@gmail.com>
2026-02-21 16:30:45 +0100

    Created basic function for calculations

--- basics-01-starting-project/assets/scripts/script.js
+++ basics-01-starting-project/assets/scripts/script.js
@@ -3,3 +3,21 @@ let result = 0;
 result = result + 10;
 
 outputResult(result, '');
+
+// Funzioni
+
+function sumFunction(num1, num2) {
+  return num1 + num2;
+}
+
+function subFunction(num1, num2) {
+  return num1 - num2;
+}
+
+function multFunction(num1, num2) {
+  return num1 * num2;
+}
+
+function divideFunction(num1, num2) {
+  return num1 / num2;
+}

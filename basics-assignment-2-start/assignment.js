ec8594d8a5f41118d12e415c04eea93e36d29902
Mykola Dekusar <ndekusar97@gmail.com>
2026-02-23 14:52:35 +0100

    Implement functions for alerts and string concatenation in assignment.js

--- basics-assignment-2-start/assignment.js
+++ basics-assignment-2-start/assignment.js
@@ -1,1 +1,20 @@
 const task3Element = document.getElementById('task-3');
+
+function empty(){
+    alert("I'm an empty function!");
+}
+
+function nameOutput(name){
+    alert(`Hello, ${name}!`)
+}
+
+function stringConcat(string1, string2, string3){
+    return string1 + " " + string2 + " " + string3;
+}
+
+empty();
+nameOutput("Nico");
+
+task3Element.addEventListener("click", empty);
+
+alert(stringConcat("Ciao", "sono,", "Nico!"));

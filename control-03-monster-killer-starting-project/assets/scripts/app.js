f217297be132e6a6388e2d4c0d859080d4c81b26
Mykola Dekusar <ndekusar97@gmail.com>
2026-02-26 15:08:18 +0100

    Refactor attackHandler function to enhance battle logic and improve health management

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,27 +1,54 @@
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 15;
 let chosenMaxLife = 10;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+const battleLog = [];
+let battleCounter = 0;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-  const damageDealt = dealMonsterDamage(ATTACK_VALUE);
-  const damageTaken = dealPlayerDamage(ATTACK_VALUE);
-  currentMonsterHealth -= damageDealt; 
-  currentPlayerHealth-= damageTaken
-
-
-  if (currentMonsterHealth <= 0) {
-    setTimeout(() => {
-      alert("Player wins");
-    }, 100);
-  } else if(currentPlayerHealth <= 0) {
-    setTimeout(() => {
-      alert("Monster wins");
-    }, 100);
-  }
- 
+function gameLog(playerHeath, monsterHealth) {
+  battleLog.push({
+    playerHealth: playerHeath,
+    playerDamage: ATTACK_VALUE,
+    monsterDamage: MONSTER_ATTACK_VALUE,
+    monsterHealth: monsterHealth,
+    battleRounds: battleCounter,
+    battleResult: 'unknown',
+  });
 }
+
+function showResult(result) {
+  setTimeout(() => {
+    alert(result);
+  }, 100);
+}
+
+function attackHandler() {
+  const draw = "Draw";
+  const pWins = "Player Wins";
+  const mWins = "Monster Wins";
+  const damageDealt = dealMonsterDamage(ATTACK_VALUE);
+  currentMonsterHealth -= damageDealt;
+  const damageTaken = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= damageTaken;
+  battleCounter++;
+
+  battleLog.push(gameLog(currentPlayerHealth, currentMonsterHealth));
+
+  if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+    battleLog[battleCounter].battleResult = draw;
+    showResult("Draw");
+  } else if (currentPlayerHealth <= 0) {
+    battleLog[battleCounter].battleResult = pWins;
+    showResult("Monster wins");
+  } else if (currentMonsterHealth <= 0) {
+    battleLog[battleCounter].battleResult = mWins;
+    showResult("Player wins");
+  }
+  console.log(battleLog);
+}
+
 
 attackBtn.addEventListener("click", attackHandler);

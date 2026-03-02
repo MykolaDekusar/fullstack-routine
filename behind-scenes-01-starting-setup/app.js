b33aa6480ba4cde28398734dbb3562ea736cf7f4
Mykola Dekusar <ndekusar97@gmail.com>
2026-03-02 19:44:59 +0100

    Refactor greet function to include variable mioNome in logging output

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -1,10 +1,16 @@
+// Le variabili let e const sono block e function scoped
 let myName = "Nico";
+// Ricreare let myName = "Marco" ci darebbe errore qua
+var mioNome = "Nico";
+// Con var non mi da errore
+var mioNome = "Marco";
+
 function greet(){
   //L'eta è bloccata nello scope della function
   let age = 28;
   // Creiamo una variabile shadow
   let myName = "Marco";
-  console.log(myName, age);
+  console.log(myName, mioNome, age);
 }
-console.log(myName);
+console.log(myName, mioNome);
 greet();

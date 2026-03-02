70809ec036009ade07460eeb656b81a52f4e1250
Mykola Dekusar <ndekusar97@gmail.com>
2026-03-02 19:52:17 +0100

    Refactor greet function to include hobbies and clarify variable scoping

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -2,15 +2,25 @@
 let myName = "Nico";
 // Ricreare let myName = "Marco" ci darebbe errore qua
 var mioNome = "Nico";
-// Con var non mi da errore
+// Con var non mi da errore se modifico la stessa variabile mioNome
 var mioNome = "Marco";
 
-function greet(){
+if (mioNome === "Marco") {
+  var hobbies = ["Gaming", "Gym"];
+  console.log(hobbies);
+}
+
+function greet() {
   //L'eta è bloccata nello scope della function
   let age = 28;
+  //Il var dentro una funzione é bloccato dentro di essa
+  var eta = 30;
   // Creiamo una variabile shadow
   let myName = "Marco";
-  console.log(myName, mioNome, age);
+  // La variabile var anche se create dentro il blocco if rimane globale
+  console.log(myName, mioNome, age, hobbies);
 }
-console.log(myName, mioNome);
+// La variabile var anche se create dentro il blocco if rimane globale invece se creato dentro
+// la funzione rimane bloccato dentro la funzione
+console.log(myName, mioNome, hobbies, eta);
 greet();

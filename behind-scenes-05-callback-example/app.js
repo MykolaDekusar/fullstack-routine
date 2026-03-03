7670932ad04d02733a189d43b1fee2978fa4ea2a
Mykola Dekusar <ndekusar97@gmail.com>
2026-03-03 15:29:21 +0100

    Add comments to clarify memory management in event listeners and experimenting with memory leaks

--- behind-scenes-05-callback-example/app.js
+++ behind-scenes-05-callback-example/app.js
@@ -1,7 +1,8 @@
 const addListenerBtn = document.getElementById('add-listener-btn');
 const clickableBtn = document.getElementById('clickable-btn');
 const messageInput = document.getElementById('click-message-input');
-
+// Facendo cosi va tutto bene, non abbiamo memory leaks perche il browser sa gestire
+// le funzioni
 function printMessage() {
   const value = messageInput.value;
   console.log(value || 'Clicked me!');
@@ -12,3 +13,15 @@ function addListener() {
 }
 
 addListenerBtn.addEventListener('click', addListener);
+// se clicchiamo piu volte addListenerBtn gli assegnamo piu volte la funzione
+// anonima allocando sempre spazio vuoto per essa ed occupando memoria
+addListenerBtn.addEventListener('click', leakedListener);
+
+// Se invece dovessimo creare un listener con un assegnazione di una funzione anonima
+// le funzioni diventerebbero molteplici e avremmo molti memory leaks
+function leakedListener(){
+  clickableBtn.addEventListener('click', function () {
+    const value = 'Leaked function';
+    console.log(value || 'Leaked');
+  })
+}

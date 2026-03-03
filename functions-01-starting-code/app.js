cc6552cd7bf69e83c0fcc72d6cf44b5528fa0f79
Mykola Dekusar <ndekusar97@gmail.com>
2026-03-03 16:18:05 +0100

    Implement startGame function and enhance person object with greet method

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -1,1 +1,16 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame(){
+  console.log("Game Started!!!");
+}
+
+const person = {
+name: "Nico",
+greet: function (){
+  console.log(this.name);
+}
+}
+
+person.greet();
+console.log(person.name);
+startGameBtn.addEventListener('click', startGame);

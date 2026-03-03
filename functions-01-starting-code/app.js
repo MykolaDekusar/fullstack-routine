ea9aed2ceec73af15d0644e243ad846485760d17
Mykola Dekusar <ndekusar97@gmail.com>
2026-03-03 16:33:20 +0100

    Testing assigning a function to a variable and creating an anonymous function

--- functions-01-starting-code/app.js
+++ functions-01-starting-code/app.js
@@ -1,20 +1,25 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById("start-game-btn");
 
-function startGame(){
+function startGame() {
   console.log("Game Started!!!");
 }
+// Possiamo anche assegnare le funzioni a delle variabili pero dovremmo chiamarla
+// per il nome della sua variabile che è start
+const start = function startGame() {
+  console.log("Game Started!!!");
+};
 
 const person = {
-name: "Nico",
-//Una funzione dentro un oggetto diventa un metodo dell'oggetto
-greet: function (){
-  console.log(this.name);
-}
-}
+  name: "Nico",
+  //Una funzione dentro un oggetto diventa un metodo dell'oggetto
+  greet: function () {
+    console.log(this.name);
+  },
+};
 //Una funzione dentro un oggetto diventa un metodo dell'oggetto
 person.greet();
 console.log(person.name);
 // In questo caso addEventListener è un metodo dell'oggetto startGameBtn
-// è diventato oggetto grazie a getElementById ottenuto dall document che è una variabile 
+// è diventato oggetto grazie a getElementById ottenuto dall document che è una variabile
 // globale che ci da accesso al documento
-startGameBtn.addEventListener('click', startGame);
+startGameBtn.addEventListener("click", start);

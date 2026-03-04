540866547d8a30d98b6fd22407b1d466dd731f87
Mykola Dekusar <ndekusar97@gmail.com>
2026-03-04 23:40:42 +0100

    Implemented rock paper scissors game logic

--- functions-04-game-user-choice/app.js
+++ functions-04-game-user-choice/app.js
@@ -5,8 +5,9 @@ const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
 let gameIsRunning = false;
+const gameChoices = [ROCK, PAPER, SCISSORS];
 
-const getPlayerChoice = function() {
+function getPlayerChoice() {
   const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
   if (
     selection !== ROCK &&
@@ -19,6 +20,34 @@ const getPlayerChoice = function() {
   return selection;
 };
 
+function getComputerChoice(){
+  const randomNumber = Math.floor(Math.random() * gameChoices.length);
+  return gameChoices[randomNumber];
+}
+
+function gameLogic(player, computer){
+  if(player === computer){
+    console.log(`Player choose ${player}\nComputer choose ${computer}\nDRAW`);
+    return;
+  }
+  if(player === ROCK && computer === SCISSORS){
+    console.log(`Player choose ${player}\nComputer choose ${computer}\nPlayer WINS`);
+    return;
+  }
+  if(player === SCISSORS && computer === PAPER){
+    console.log(`Player choose ${player}\nComputer choose ${computer}\nPlayer WINS`);
+    return;
+  }
+  if(player === PAPER && computer === ROCK){
+    console.log(`Player choose ${player}\nComputer choose ${computer}\nPlayer WINS`);
+    return;
+  }
+  console.log(`Player choose ${player}\nComputer choose ${computer}\nComputer WINS`);
+  return;
+}
+
+
+
 startGameBtn.addEventListener('click', function() {
   if(gameIsRunning){
     return;
@@ -26,6 +55,9 @@ startGameBtn.addEventListener('click', function() {
   gameIsRunning = true;
   console.log('Game is starting...');
   const playerSelection = getPlayerChoice();
-  console.log(playerSelection);
-  
+  const computerSelecton = getComputerChoice();
+
+  console.log(playerSelection, computerSelecton);
+  gameLogic(playerSelection,computerSelecton);
+  gameIsRunning = false;
 });

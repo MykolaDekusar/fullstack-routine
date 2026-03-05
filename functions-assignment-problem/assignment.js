86bd9629cdb1ccc82badad62d7b53e12090ff0a3
Mykola Dekusar <ndekusar97@gmail.com>
2026-03-05 15:40:31 +0100

    Exercise with various form of functions

--- functions-assignment-problem/assignment.js
+++ functions-assignment-problem/assignment.js
@@ -1,5 +1,32 @@
-function sayHello(name) {
-  console.log('Hi ' + name);
+const NAME = "Nico";
+const PHRASE = "Hello there ";
+
+const sayHello = name => console.log("Hi " + name + "!");
+
+function newSayHello(phrase, name) {
+  console.log(phrase + name + "!");
 }
 
-sayHello();
+function hardCodedHello() {
+  console.log("Hello  there Nico!");
+}
+
+const outputHello = (name ,phrase = "I'm default, hello ") => phrase + name;
+
+const checkInput = (allGood, ...dati) =>{
+  for(let data of dati){
+    if(!data){
+      return;
+    }
+  }
+  allGood();
+};
+
+const allGood = () => console.log("Tutto a posto");
+
+sayHello(NAME);
+newSayHello(PHRASE, NAME);
+hardCodedHello();
+console.log(outputHello(NAME));
+checkInput(allGood, PHRASE, NAME, "Ma se non lo fosse? ");
+

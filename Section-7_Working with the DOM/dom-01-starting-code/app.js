7851bb60b48a60652d8047d6decda65eb8a2ae83
Mykola Dekusar <ndekusar97@gmail.com>
2026-03-06 16:50:48 +0100

    Explaining document and window

---
+++ Section-7_Working with the DOM/dom-01-starting-code/app.js
@@ -0,0 +1,7 @@
+// document fa parte di window
+// entrambi anno dei metodi
+window.alert();
+// per adesso l'abbiamo scritto in questo modo
+alert();
+// ovviamente ci sono moltissimi metodi
+console.dir(document);

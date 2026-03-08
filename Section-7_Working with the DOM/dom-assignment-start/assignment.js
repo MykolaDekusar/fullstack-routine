0416bef61ca984fb0e43e2fd0b131f80b2010a22
Mykola Dekusar <ndekusar97@gmail.com>
2026-03-08 16:48:42 +0100

    First task done

---
+++ Section-7_Working with the DOM/dom-assignment-start/assignment.js
@@ -0,0 +1,7 @@
+// const firstTask = document.getElementById('task-1');
+// firstTask.style.backgroundColor = 'black';
+// firstTask.style.color = 'white';
+
+const firstTask2 = document.querySelector('#task-1');
+firstTask2.style.backgroundColor= 'black';
+firstTask2.style.color= 'white';

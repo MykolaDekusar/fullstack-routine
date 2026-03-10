99b2cbc8020ebc978db372b12e3273b7fa8beb88
Mykola Dekusar <ndekusar97@gmail.com>
2026-03-10 16:15:07 +0100

    Using toggle instead of add and remove

--- Section-7_Working with the DOM/dom-06-demo-project-setup/assets/scripts/app.js
+++ Section-7_Working with the DOM/dom-06-demo-project-setup/assets/scripts/app.js
@@ -1,14 +1,16 @@
 const addMoviebutton = document.querySelector("header button");
 const modal = document.getElementById("add-modal");
-let modalManager = false;
+//let modalManager = false;
 
 addMoviebutton.addEventListener("click", () => {
-  if (!modalManager) {
-    console.log("ciao");
-    modal.classList.add("visible");
-    modalManager = true;
-  } else {
-    modal.classList.remove("visible");
-    modalManager = false;
-  }
+  // Possiamo usare questo modo
+  // if (!modalManager) {
+  //   modal.classList.add("visible");
+  //   modalManager = true;
+  // } else {
+  //   modal.classList.remove("visible");
+  //   modalManager = false;
+  // }
+  // Oppure il .toggle()
+  modal.classList.toggle('visible');
 });

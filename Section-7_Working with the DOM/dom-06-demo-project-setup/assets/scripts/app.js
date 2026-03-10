6cd609f705ec2d828a34a5b0b4934b21a229ea2f
Mykola Dekusar <ndekusar97@gmail.com>
2026-03-10 16:29:03 +0100

    Implemented addMovie function and refactored code

--- Section-7_Working with the DOM/dom-06-demo-project-setup/assets/scripts/app.js
+++ Section-7_Working with the DOM/dom-06-demo-project-setup/assets/scripts/app.js
@@ -1,8 +1,10 @@
 const addMoviebutton = document.querySelector("header button");
 const modal = document.getElementById("add-modal");
 const backDrop = document.getElementById("backdrop");
-const cancelModal = document.querySelector(".modal__actions");
+const cancelModal = document.querySelector(".modal__actions .btn--passive");
+const addButton = document.querySelector(".modal__actions .btn--success");
 
+// Logic of showing and removing modal and backdrop
 cancelModal.addEventListener("click", () => {
   toggleModal();
   showBackDrop();
@@ -13,10 +15,25 @@ addMoviebutton.addEventListener("click", () => {
   showBackDrop();
 });
 
+backDrop.addEventListener("click", ()=>{
+  toggleModal();
+  showBackDrop();
+});
+
+addButton.addEventListener("click", ()=>{
+  addMovie();
+})
+
 function showBackDrop() {
   backDrop.classList.toggle("visible");
 }
 
 function toggleModal() {
   modal.classList.toggle("visible");
+}
+
+function addMovie(){
+  alert("Il film è stato aggiunto con successo!");
+  toggleModal();
+  showBackDrop();
 }

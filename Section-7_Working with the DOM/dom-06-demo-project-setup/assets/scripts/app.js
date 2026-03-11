daac6e0d2fe7fea5799c4de095e7b7079fb1a7d3
Mykola Dekusar <ndekusar97@gmail.com>
2026-03-11 15:07:18 +0100

    Created function that adds the added movie to screen

--- Section-7_Working with the DOM/dom-06-demo-project-setup/assets/scripts/app.js
+++ Section-7_Working with the DOM/dom-06-demo-project-setup/assets/scripts/app.js
@@ -3,7 +3,8 @@ const modal = document.getElementById("add-modal");
 const backDrop = document.getElementById("backdrop");
 const cancelModal = document.querySelector(".modal__actions .btn--passive");
 const addButton = document.querySelector(".modal__actions .btn--success");
-const addMovieToList = document.getElementById('movie-list');
+const addMovieToList = document.getElementById("movie-list");
+const movieSection = document.getElementById("entry-text");
 // Selecting user inputs
 const userInputs = modal.querySelectorAll("input");
 
@@ -44,20 +45,37 @@ function addMovie() {
   const movieImage = userInputs[1].value;
   const movieRating = userInputs[2].value;
 
-  if (movieTitle && movieImage && (+movieRating >= 1 && +movieRating <= 5)) {
+  if (movieTitle && movieImage && +movieRating >= 1 && +movieRating <= 5) {
     movieList.push({ movieTitle, movieImage, movieRating });
     alert("Il film è stato aggiunto con successo!");
     toggleModal();
     showBackDrop();
+    createMovieCard(movieTitle, movieImage, movieRating);
   } else {
     alert("Inserisci valori validi, il rating deve essere tra 1 e 5");
     return;
   }
 }
 
+function clearUserInput() {
+  for (const userInput of userInputs) {
+    userInput.value = "";
+  }
+}
 
-function clearUserInput(){
-  for(const userInput of userInputs){
-    userInput.value = '';
+function createMovieCard(title, image, rating) {
+  if (movieList.length) {
+    movieSection.classList.add("hide");
+    const newMovieElement = document.createElement("li");
+    newMovieElement.className = "movie-element";
+    newMovieElement.innerHTML = `
+    <div class='movie-element__info'>
+      <h2>Title: ${title}</h2>
+      <p>Rating: ${rating}\\5 stars</p>
+    </div>
+    <div class='movie-element__image'>
+      <img src = '${image}' alt = '${title}'>
+    </div>`;
+    addMovieToList.appendChild(newMovieElement);
   }
 }

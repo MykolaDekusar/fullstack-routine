fce50a096d561b793e0954dbe40f0438ead486b8
Mykola Dekusar <ndekusar97@gmail.com>
2026-03-14 16:05:14 +0100

    Explanation of Map and how to get and set new values

--- Section-8_More on Arrays & Iterables/arrays-01-starting-code/setsmaps.js
+++ Section-8_More on Arrays & Iterables/arrays-01-starting-code/setsmaps.js
@@ -1,4 +1,4 @@
-// Per creare un nuovo set dobbiamo usare sempre new Set();
+// Per creare un nuovo set dobbiamo usare sempre 
 // I Set non permettono duplicati e ci permettono di gestire valori unici
 const ids = new Set([1,2,3]);
 console.log(ids); //Set(3) [ 1, 2, 3 ]
@@ -13,3 +13,33 @@ for (const entry of ids.entries()){
   console.log(entry);//Array [ 1, 1 ] Array [ 2, 2 ]
   console.log(entry[0]);// 1  2
 }
+
+//****************************************************
+// Usiamo la map quando abbiamo un oggetto che non vogliamo modificare
+// Ma al quale vogliamo aggiungere dei dati o informazioni VALUE
+// Per esempio se lo usiamo da altre parti e non vogliamo modificare l'oggetto originale
+const person1 = {name: "Nico"};
+const person2 = {name: "Marco"};
+
+// new Map([['key','some values']])
+const peopleData = new Map([[person1,[{date:'21st March', price: 10}]]]);
+console.log(peopleData);
+/* Object { name: "Nico" } → Array [ {…} ]
+    <key>: Object { name: "Nico" }
+    <value>: Array [ {…} ]
+      0: Object { date: "21st March", price: 10 } */
+console.log(peopleData.get(person1));
+/* Array [ {…} ]
+     0: Object { date: "21st March", price: 10 }
+ */
+
+// Possiamo anche aggiungere dati ad una Map settata
+
+peopleData.set(person2, [{date: "6 December", price: 23}]);
+/*  0: Object { name: "Nico" } → Array [ {…} ]
+    1: Object { name: "Marco" } → Array [ {…} ]
+        <key>: Object { name: "Marco" }
+        <value>: Array [ {…} ]
+            0: Object { date: "6 December", price: 23 }*/
+
+//****************************************************         

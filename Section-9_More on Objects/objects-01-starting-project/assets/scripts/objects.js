b70e5dc153f4fc7aa394dbffa94c3e6d8e8b4e7a
Mykola Dekusar <ndekusar97@gmail.com>
2026-03-15 14:49:45 +0100

    Defined dynamic setting of keys using square brakets

--- Section-9_More on Objects/objects-01-starting-project/assets/scripts/objects.js
+++ Section-9_More on Objects/objects-01-starting-project/assets/scripts/objects.js
@@ -1,4 +1,6 @@
 // Objects recap
+// le chiavi degli oggeti vengono automaticamente interpretati come stringhe da
+const userChosenKey = "height"; // Esempio
 const person = {
   //key: value
   //first-name: 'Nico' CI DA ERRORE PERCHE FIRST-NAME NON E' VALIDO
@@ -10,6 +12,10 @@ const person = {
   greet: function () {
     console.log(`Hi there I'm ${this.name}`);
   },
+  //Possiamo anche assegnare numeri POSITIVI come chiavi
+  1.5: "Hello",
+  // Possiamo anche dinamicamente settare le chiavi dell'oggetto
+  [userChosenKey]: '',
 };
 
 person.greet();
@@ -27,6 +33,12 @@ delete person.isMarried;
 // Oppure person.isMarried = undefined; PERO NON E' PULITO
 
 // Vediamo come accedere alle key speciali come stringa "first-name"
-// Dobbiamo passare una stringa tra le quadre, altrimenti cerca una variabile
+// Dobbiamo passare una stringa tra le quadre, altrimenti cerca una variabile nel codice da passare
 console.log(person["age"]);
 console.log(person["first-name"]);
+console.log(person[1.5]);
+console.log(person["1.5"]);
+
+// Possiamo anche accedere dinamicamente ai valori 
+const keyName = "first-name";
+console.log(person[keyName]);

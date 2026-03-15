6df2fe22d6cccdbc990eea31bcac3ee971217090
Mykola Dekusar <ndekusar97@gmail.com>
2026-03-15 14:24:40 +0100

    Using special keys and defining how to access them

--- Section-9_More on Objects/objects-01-starting-project/assets/scripts/objects.js
+++ Section-9_More on Objects/objects-01-starting-project/assets/scripts/objects.js
@@ -1,18 +1,21 @@
 // Objects recap
 const person = {
-//key: value
-  name: 'Nico',
+  //key: value
+  //first-name: 'Nico' CI DA ERRORE PERCHE FIRST-NAME NON E' VALIDO
+  //POSSIAMO PERO FARE COSI
+  "first-name": "Luca",
+  name: "Nico",
   age: 28,
-  hobbies: ['Racing', 'Gaming'],
-  greet: function (){
+  hobbies: ["Racing", "Gaming"],
+  greet: function () {
     console.log(`Hi there I'm ${this.name}`);
-  }
+  },
 };
 
 person.greet();
 
 // Vediamo come modificare i valori di un oggetto'
-person.name = 'Max';
+person.name = "Max";
 console.log(person);
 
 // E come aggiungere una key: value
@@ -21,3 +24,9 @@ console.log(person); //Object { name: "Max", age: 28, hobbies: (2) […], greet:
 
 // E come eliminare una proprieta dall' Object
 delete person.isMarried;
+// Oppure person.isMarried = undefined; PERO NON E' PULITO
+
+// Vediamo come accedere alle key speciali come stringa "first-name"
+// Dobbiamo passare una stringa tra le quadre, altrimenti cerca una variabile
+console.log(person["age"]);
+console.log(person["first-name"]);

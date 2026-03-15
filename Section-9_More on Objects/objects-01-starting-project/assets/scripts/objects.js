f537d77ece4b615b6867e9bf4e8397a8d6033a75
Mykola Dekusar <ndekusar97@gmail.com>
2026-03-15 14:15:18 +0100

    Testing updating, adding and removing properties from Object

--- Section-9_More on Objects/objects-01-starting-project/assets/scripts/objects.js
+++ Section-9_More on Objects/objects-01-starting-project/assets/scripts/objects.js
@@ -5,8 +5,19 @@ const person = {
   age: 28,
   hobbies: ['Racing', 'Gaming'],
   greet: function (){
-    alert(`Hi there I'm ${this.name}`);
+    console.log(`Hi there I'm ${this.name}`);
   }
 };
 
 person.greet();
+
+// Vediamo come modificare i valori di un oggetto'
+person.name = 'Max';
+console.log(person);
+
+// E come aggiungere una key: value
+person.isMarried = false;
+console.log(person); //Object { name: "Max", age: 28, hobbies: (2) […], greet: greet(), isMarried: false }
+
+// E come eliminare una proprieta dall' Object
+delete person.isMarried;

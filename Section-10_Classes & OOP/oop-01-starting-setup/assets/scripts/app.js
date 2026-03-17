99e9317b31bbcf9bdf57482ae5b4f3c883f70a38
Mykola Dekusar <ndekusar97@gmail.com>
2026-03-17 16:16:56 +0100

    Created prodEl using innerHTML and appended it to UL

--- Section-10_Classes & OOP/oop-01-starting-setup/assets/scripts/app.js
+++ Section-10_Classes & OOP/oop-01-starting-setup/assets/scripts/app.js
@@ -17,15 +17,26 @@ const productList = {
   ],
   render() {
     const renderLocation = document.getElementById('app');
-    const createList = document.createElement('ul');
-    createList.className = 'product-list';
+    const prodList = document.createElement('ul');
+    prodList.className = 'product-list';
     for(const prod of this.products){
       const prodEl = document.createElement('li');
       prodEl.className = 'product-item';
-      
+      prodEl.innerHTML = `
+      <div>
+        <img src ='${prod.imageUrl}' alt = '${prod.title}'>
+        <div class='product-item__content'>
+          <h2>${prod.title}</h2>
+          <h3>\$${prod.price}</h3>
+          <p>${prod.description}</p>
+          <button>Add to Cart</button>
+        </div>
+      </div>
+      `
+      prodList.append(prodEl);
     }
     
-    renderLocation.append();
+    renderLocation.append(prodList);
   }
 };
 

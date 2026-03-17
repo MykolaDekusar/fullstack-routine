8fcfadea28af4b2b39acd5dbed6308a3c8b92d6c
Mykola Dekusar <ndekusar97@gmail.com>
2026-03-17 16:47:05 +0100

    Splitted logic in more Classes

--- Section-10_Classes & OOP/oop-01-starting-setup/assets/scripts/app.js
+++ Section-10_Classes & OOP/oop-01-starting-setup/assets/scripts/app.js
@@ -13,8 +13,33 @@ class Product {
   }
 }
 
-const productList = {
-  products: [
+class ProductItemRender {
+  constructor(product){
+    this.product = product;
+  }
+
+  render(){
+    const prodEl = document.createElement("li");
+    const prod = this.product;
+      prodEl.className = "product-item";
+      prodEl.innerHTML = `
+      <div>
+        <img src ='${prod.imageUrl}' alt = '${prod.title}'>
+        <div class='product-item__content'>
+          <h2>${prod.title}</h2>
+          <h3>\$${prod.price}</h3>
+          <p>${prod.description}</p>
+          <button>Add to Cart</button>
+        </div>
+      </div>
+      `;
+    return prodEl;
+  }
+}
+
+// Creaimo una classe che contiene la logica
+class ProductList {
+  products = [
     // Utilizziamo la classe Product per creare un nuovo prodotto
     // Grazie al constructor
     new Product(
@@ -29,44 +54,80 @@ const productList = {
       499.99,
       "Persian premium carpet",
     ),
-    // {
-    //   title: "A pillow",
-    //   imageUrl:
-    //     "https://m.media-amazon.com/images/I/91ZmVcNJh9L._AC_UF894,1000_QL80_.jpg",
-    //   price: 19.99,
-    //   description: "A super soft comfy pillow",
-    // },
-    // {
-    //   title: "Carpet",
-    //   imageUrl:
-    //     "https://www.loomkart.com/cdn/shop/files/fauxsilkcarpetloomkart_neosilk550017_3.jpg?v=1753537930",
-    //   price: 499.99,
-    //   description: "Persian premium carpet",
-    // },
-  ],
+  ];
+  constructor() {}
+  // Passiamo qua dentro la logica
   render() {
     const renderLocation = document.getElementById("app");
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
     for (const prod of this.products) {
-      const prodEl = document.createElement("li");
-      prodEl.className = "product-item";
-      prodEl.innerHTML = `
-      <div>
-        <img src ='${prod.imageUrl}' alt = '${prod.title}'>
-        <div class='product-item__content'>
-          <h2>${prod.title}</h2>
-          <h3>\$${prod.price}</h3>
-          <p>${prod.description}</p>
-          <button>Add to Cart</button>
-        </div>
-      </div>
-      `;
+      const productItem = new ProductItemRender(prod);
+      const prodEl = productItem.render()
       prodList.append(prodEl);
     }
 
     renderLocation.append(prodList);
-  },
-};
+  }
+}
+
+// const productList = {
+//   products: [],
+// products: [
+// Utilizziamo la classe Product per creare un nuovo prodotto
+// Grazie al constructor
+// new Product(
+//   "A pillow",
+//   "https://m.media-amazon.com/images/I/91ZmVcNJh9L._AC_UF894,1000_QL80_.jpg",
+//   19.99,
+//   "A super soft comfy pillow",
+// ),
+// new Product(
+//   "Carpet",
+//   "https://www.loomkart.com/cdn/shop/files/fauxsilkcarpetloomkart_neosilk550017_3.jpg?v=1753537930",
+//   499.99,
+//   "Persian premium carpet",
+// ),
+// {
+//   title: "A pillow",
+//   imageUrl:
+//     "https://m.media-amazon.com/images/I/91ZmVcNJh9L._AC_UF894,1000_QL80_.jpg",
+//   price: 19.99,
+//   description: "A super soft comfy pillow",
+// },
+// {
+//   title: "Carpet",
+//   imageUrl:
+//     "https://www.loomkart.com/cdn/shop/files/fauxsilkcarpetloomkart_neosilk550017_3.jpg?v=1753537930",
+//   price: 499.99,
+//   description: "Persian premium carpet",
+// },
+// ],
+// render() {
+//   const renderLocation = document.getElementById("app");
+//   const prodList = document.createElement("ul");
+//   prodList.className = "product-list";
+//   for (const prod of this.products) {
+//     const prodEl = document.createElement("li");
+//     prodEl.className = "product-item";
+//     prodEl.innerHTML = `
+//     <div>
+//       <img src ='${prod.imageUrl}' alt = '${prod.title}'>
+//       <div class='product-item__content'>
+//         <h2>${prod.title}</h2>
+//         <h3>\$${prod.price}</h3>
+//         <p>${prod.description}</p>
+//         <button>Add to Cart</button>
+//       </div>
+//     </div>
+//     `;
+//     prodList.append(prodEl);
+//   }
+
+//   renderLocation.append(prodList);
+// },
+// };
+
+const productList = new ProductList();
 
 productList.render();

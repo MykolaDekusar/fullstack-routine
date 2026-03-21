8d07cc05220a133ef273511e3df4e0681d6842b6
Mykola Dekusar <ndekusar97@gmail.com>
2026-03-21 15:08:57 +0100

    approfondimento sulle meccaniche dei prototype e costruttori in JS
    - Dimostrato l'effetto della chiamata a una funzione costruttore senza la keyword 'new' (assegnazione undefined).
    - Risolto l'errore di referenza creando una nuova istanza (persona2) solo dopo la riassegnazione di Persona.prototype.
    - Aggiunti log per confrontare la prototype chain tra le istanze create prima e dopo la modifica del prototype.

--- Section-11_Deep Dive Constructor Functions & Prototypes/prototypes-01-starting-setup/app.js
+++ Section-11_Deep Dive Constructor Functions & Prototypes/prototypes-01-starting-setup/app.js
@@ -28,8 +28,55 @@ function Persona() {
 }
 // La cosa importane è la parola NEW che ci permette di fare queste cose
 // Altrimenti sarebbe solo una funzione void che non ritorna nulla
-const person1 = new Persona();
-person1.greet();
+const persona = new Persona();
+persona.greet();
 
 const person2 = Persona(); // Gli assegno una funzione che non ha return quindi undefined
-person2.greet(); //can't access property "greet", person2 is undefined
+if(person2){
+  person2.greet(); //can't access property "greet", person2 is undefined
+} else {
+  console.warn("undefined");
+}
+
+
+// Ogni oggetto ha un prototype
+console.log(persona); 
+/*
+age: 28
+greet: function greet()
+name: "Nico"
+<prototype>: Object { … }
+*/
+
+console.log(persona.__proto__); 
+/*
+Object { … }
+  constructor: function Persona()
+  <prototype>: Object { … }
+ */
+console.log(person.__proto__ === Person.prototype); // true Sono ESATTAMENTE lo stesso oggetto
+
+// Ogni oggetto basato sulla classe Person avra un prototipe che è esattamente questo oggetto
+Persona.prototype = {
+  printAge(){
+    console.log(this.age);
+  }
+}
+// Dobbiamo creare una nuova persona 
+const persona2 = new Persona();
+
+persona2.printAge();// 28
+console.log(persona2.__proto__); /*
+  Object { printAge: printAge() }
+    printAge: function printAge()
+*/
+
+console.log(persona.__proto__); 
+/*
+constructor: function Persona()
+  arguments: null
+  caller: null
+  length: 0
+  name: "Persona"
+  prototype: Object { printAge: printAge() } 
+*/

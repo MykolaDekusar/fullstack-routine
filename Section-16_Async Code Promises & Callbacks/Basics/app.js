ba5baf2777bb722b520ee265d990f0f503d479c9
Mykola Dekusar <ndekusar97@gmail.com>
2026-03-31 20:09:13 +0200

    Created a promise function that uses resolve and used then

--- Section-16_Async Code Promises & Callbacks/Basics/app.js
+++ Section-16_Async Code Promises & Callbacks/Basics/app.js
@@ -6,15 +6,26 @@ const button = document.querySelector("button");
 function getUserLocation() {
   navigator.geolocation.getCurrentPosition(
     (posData) => {
-      console.log(posData);
+      setTimer(2000).then((data) => {
+        console.log(data, posData);
+      });
     },
     (error) => console.log(error),
   );
   //anche se settassimo setTimeout a 0 esso andra nell'event loop ritardando l'esecuzione del codice
-  setTimeout(() =>{
+  setTimeout(() => {
     console.log("I'm timed to 0");
   }, 0); // 0 non è il tempo garantito, è il tempo minimo possibile
-  console.log('Getting Position...'); // Verra mostato subito perche navigator richiede tempo ed è async
+  console.log("Getting Position..."); // Verra mostato subito perche navigator richiede tempo ed è async
 }
 
 button.addEventListener("click", getUserLocation);
+
+function setTimer(duration) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, duration);
+  });
+  return promise;
+}
